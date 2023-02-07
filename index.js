a9aae89a2b598c20f4e2fe09deb9e2efeabd85ab
unknown <nuzaim.t123@gmail.com>
2023-02-07 08:33:41 +0530

    Mongoose Part 1

---
+++ index.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Dishes = require("./models/dishes");
+
+const url = "mongodb://127.0.0.1:27017/conFusion";
+const connect = mongoose.connect(url);
+
+connect.then((db)=>{
+    console.log("Connected to the server successfully!");
+
+    var newDish = Dishes({
+        name : "pasta",
+        description : "with white sauce"
+    });
+
+    newDish.save()
+        .then((dish)=>{
+            console.log(dish);
+            return Dishes.find({});
+        })
+        .then((dishes)=>{
+            console.log(dishes);
+            return Dishes.deleteMany({});
+        }).then(()=>{
+            return mongoose.connection.close();
+        }).catch((err)=>{
+            console.log(err);
+        });
+});
